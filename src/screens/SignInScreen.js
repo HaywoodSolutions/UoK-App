ebcb433fc0551450ee8eb27d1af12fe2e0c8cd7e
Unknown <thomashaywoodproductions@gmail.com>
2018-11-27 20:36:36 +0000

    Updated login screen

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {View, Text, StyleSheet, Platform} from 'react-native';
+import {View, Text, StyleSheet, Platform, SafeAreaView } from 'react-native';
 import {connect} from 'react-redux';
 import {Button, Input} from '../components';
 import {authUser, emailChanged, passwordChanged} from '../actions';
 import {THEME_COLOR} from "../lib/Constants";
+import { Video } from 'expo';
 
 class SignIn extends React.Component {
   static navigationOptions = {
@@ -29,56 +30,67 @@ class SignIn extends React.Component {
 
     return (
         <View style={backgroundStyle}>
-          <Text
-              style={{
-                fontWeight: '600',
-                fontSize: 30,
-                alignSelf: 'center',
-                color: 'white',
-                textAlign: 'center',
-                paddingTop: 10
-              }}
-          >
-            {'KentFlix'}
-          </Text>
-          <Text
-            style={labelStyle}>
-            Email
-          </Text>
-          <Input
-              placeholder='ab123@kent.ac.uk'
-              style={inputStyle}
-              value={email}
-              onChangeText={(email) => this.props.emailChanged(email)}
+          <Video
+            source={require('../videos/bg.mp4')}
+            resizeMode="cover"
+            shouldPlay
+            isMuted={true}
+            shouldPlay
+            isLooping    
+            style={styles.video}
           />
-          <Text style={labelStyle}>
-            Password
-          </Text>
-          <Input
-              placeholder='password'
-              style={inputStyle}
-              value={password}
-              secureTextEntry={true}
-              onChangeText={(password) => this.props.passwordChanged(password)}
-          />
-          <Button
-              title={loading ? '' : 'Sign In'}
-              style={{
-                height: 60,
-                margin: 10,
-                paddingTop: 5,
-                paddingBottom: 5
-              }}
-              textStyle={{
-                fontSize: 20,
-                color: THEME_COLOR
-              }}
-              buttonStyle={{
-                backgroundColor: '#fff'
-              }}
-              loadingColor={THEME_COLOR}
-              onPress={() => this.props.authUser(email, password, navigation)}
-              loading={loading}/>
+          <SafeAreaView   style={styles.content}>
+            <Text
+                style={{
+                  fontWeight: '600',
+                  fontSize: 30,
+                  alignSelf: 'center',
+                  color: 'white',
+                  textAlign: 'center',
+                  paddingTop: 10
+                }}
+            >
+              {'KentFlix'}
+            </Text>
+            <Text
+              style={labelStyle}>
+              Email
+            </Text>
+            <Input
+                placeholder='ab123@kent.ac.uk'
+                style={inputStyle}
+                value={email}
+                onChangeText={(email) => this.props.emailChanged(email)}
+            />
+            <Text style={labelStyle}>
+              Password
+            </Text>
+            <Input
+                placeholder='password'
+                style={inputStyle}
+                value={password}
+                secureTextEntry={true}
+                onChangeText={(password) => this.props.passwordChanged(password)}
+            />
+            <Button
+                title={loading ? '' : 'Sign In'}
+                style={{
+                  height: 60,
+                  margin: 10,
+                  paddingTop: 5,
+                  paddingBottom: 5
+                }}
+                textStyle={{
+                  fontSize: 20,
+                  color: THEME_COLOR
+                }}
+                buttonStyle={{
+                  backgroundColor: '#fff'
+                }}
+                loadingColor={THEME_COLOR}
+                onPress={() => this.props.authUser(email, password, navigation)}
+                loading={loading}/>
+          </SafeAreaView >
         </View>
     );
   }
@@ -93,6 +105,18 @@ const styles = StyleSheet.create({
         paddingTop:10
       }
     })
+  },
+  content: {
+    flex: 1,
+    justifyContent:'center',
+    alignItems: 'stretch',
+  },
+  video: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
   inputStyle: {
     backgroundColor: '#fff',
